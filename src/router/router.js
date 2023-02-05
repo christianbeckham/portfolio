523f17bb68dea2d29cd70db747eabe50cbfd23a4
Christian Beckham <urenacb22@gmail.com>
2023-02-05 17:00:57 -0600

    feat: created app routes

---
+++ src/router/router.js
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import HomePage from "../pages/HomePage/HomePage";
+import AboutPage from "../pages/AboutPage/AboutPage";
+
+export const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "/",
+				element: <HomePage />,
+			},
+			{
+				path: "about",
+				element: <AboutPage />,
+			},
+			{
+				path: "projects",
+				element: <h1>projects</h1>,
+			},
+		],
+	},
+]);
