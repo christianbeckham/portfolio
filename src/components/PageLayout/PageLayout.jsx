39357215c11bf2c851b4e8dbe188211191995e89
Christian Beckham <urenacb22@gmail.com>
2023-02-06 12:15:31 -0600

    feat: added NavBar and MobileNav components with links props

--- src/components/PageLayout/PageLayout.jsx
+++ src/components/PageLayout/PageLayout.jsx
@@ -1,7 +1,18 @@
 import { Box } from "@mui/material";
+import NavBar from "../NavBar/NavBar";
+import MobileNav from "../MobileNav/MobileNav";
+import useResponsive from "../../hooks/useResponsive";
+import { links } from "../../app.config";
 
 const PageLayout = ({ children }) => {
-	return <Box sx={{ height: "100vh" }}>{children}</Box>;
+	const { isMobile } = useResponsive();
+	return (
+		<Box sx={{ height: "100vh" }}>
+			<NavBar links={links} />
+			{children}
+			{isMobile && <MobileNav links={links} />}
+		</Box>
+	);
 };
 
 export default PageLayout;
