0f6d81c349a5996a6b7d72832923ef5564d8aa63
Christian Beckham <urenacb22@gmail.com>
2023-02-06 15:31:20 -0600

    feat: added SliderMenu component and updated styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,28 +1,32 @@
 import { Link as RouterLink } from "react-router-dom";
-import { AppBar, Toolbar, Link, Typography, Container, Stack, Divider } from "@mui/material";
+import { AppBar, Toolbar, Link, Typography, Container, Box, Stack, Divider } from "@mui/material";
 import ToggleThemeButton from "../ToggleThemeButton/ToggleThemeButton";
+import SliderMenu from "../SliderMenu/SliderMenu";
+import useResponsive from "../../hooks/useResponsive";
 
 const NavBar = ({ links }) => {
+	const { isTablet } = useResponsive();
 	return (
 		<>
 			<AppBar position="fixed" elevation={0} color={"transparent"}>
 				<Container maxWidth="xl">
 					<Toolbar component={"header"} disableGutters>
+						{isTablet && <SliderMenu links={links} />}
 						<Typography
 							variant="h6"
 							noWrap
 							component={RouterLink}
 							to="/"
 							sx={{
-								mr: 2,
-								flexGrow: 1,
+								ml: 1,
 								color: "inherit",
 								textDecoration: "none",
 							}}
 						>
 							{"{cb}"}
 						</Typography>
-						<Stack direction={"row"} spacing={3} sx={{ mx: 2, display: { xs: "none", md: "flex" } }}>
+						<Box sx={{ flexGrow: 1 }} />
+						<Stack direction={"row"} spacing={3} sx={{ mx: 2, display: { xs: "none", lg: "flex" } }}>
 							{links.slice(1).map((link) => (
 								<Link underline="none" component={RouterLink} to={link.url} key={link.title} sx={{ color: "inherit" }}>
 									{link.title}
