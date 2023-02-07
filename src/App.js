0439505b38e08553647fc8896b3f009ada54a40e
Christian Beckham <urenacb22@gmail.com>
2023-02-06 18:21:46 -0600

    refactor: brought in code from PageLayout component to reduce further nesting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import { Outlet } from "react-router-dom";
-import PageLayout from "./components/PageLayout/PageLayout";
+import { Box } from "@mui/material";
+import NavBar from "./components/NavBar/NavBar";
+import MobileNav from "./components/MobileNav/MobileNav";
+import useResponsive from "./hooks/useResponsive";
+import { links } from "./app.config";
 
 const App = () => {
+	const { isMobile } = useResponsive();
 	return (
-		<PageLayout>
-			<Outlet />
-		</PageLayout>
+		<Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+			<NavBar links={links} />
+			<Box
+				sx={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					flex: 1,
+				}}
+			>
+				<Outlet />
+			</Box>
+			{isMobile && <MobileNav links={links} />}
+		</Box>
 	);
 };
 
