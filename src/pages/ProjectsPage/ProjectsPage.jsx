01246a1cbc7f0d67014ce03a417102f8ff65fcd5
Christian Beckham <urenacb22@gmail.com>
2023-02-06 18:17:30 -0600

    feat: created project page to render project components

---
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -0,0 +1,26 @@
+import { Box, Typography } from "@mui/material";
+import LoadAnimation from "../../components/LoadAnimation/LoadAnimation";
+import ProjectsList from "../../components/ProjectsList/ProjectsList";
+
+const ProjectsPage = () => {
+	return (
+		<LoadAnimation>
+			<Box
+				component="main"
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				<Typography variant={"h3"}>projects</Typography>
+				<Box sx={{ maxWidth: 950, borderStyle: "solid", borderWidth: "1px 0", p: 2 }}>
+					<ProjectsList />
+				</Box>
+			</Box>
+		</LoadAnimation>
+	);
+};
+
+export default ProjectsPage;
