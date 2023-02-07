163fc36e06e1078ac7c18805ae29452eab1b7d73
Christian Beckham <urenacb22@gmail.com>
2023-02-07 14:15:31 -0600

    feat: updated title, added TechList component, and updated styling for better spacing

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import LoadAnimation from "../../components/LoadAnimation/LoadAnimation";
+import TechList from "../../components/TechList/TechList";
 import { myInfo } from "../../app.config";
 
 const AboutPage = () => {
@@ -14,9 +15,13 @@ const AboutPage = () => {
 					justifyContent: "center",
 				}}
 			>
-				<Typography variant={"h3"}>about</Typography>
-				<Box sx={{ maxWidth: 950, borderStyle: "solid", borderWidth: "1px 0", p: 4 }}>
-					<Typography>{myInfo.about}</Typography>
+				<Typography variant={"h3"}>about me</Typography>
+				<Box sx={{ maxWidth: 950, borderStyle: "solid", borderWidth: "1px 0", my: 1, p: 4 }}>
+					<Typography variant="h5">{`Hello, I'm ${myInfo.first_name}! 👋`}</Typography>
+					<br />
+					<Typography sx={{ my: 2 }}>{myInfo.about}</Typography>
+					<br />
+					<TechList />
 				</Box>
 			</Box>
 		</LoadAnimation>
