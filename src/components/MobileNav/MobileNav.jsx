1a61b8067ce73b2dcfad644ffd78214f10cc893c
Christian Beckham <urenacb22@gmail.com>
2023-02-08 13:02:03 -0600

    feat: updated component to display elevation and react router link component to keep consistency

--- src/components/MobileNav/MobileNav.jsx
+++ src/components/MobileNav/MobileNav.jsx
@@ -1,22 +1,22 @@
-import { Link } from "react-router-dom";
-import { Box, BottomNavigation, BottomNavigationAction, Toolbar } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { BottomNavigation, BottomNavigationAction, Toolbar, Paper } from "@mui/material";
 
 const MobileNav = ({ links }) => {
 	return (
 		<>
-			<Box sx={{ position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 1 }} elevation={3}>
+			<Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 1 }} elevation={1}>
 				<BottomNavigation showLabels>
 					{links.map((link) => (
 						<BottomNavigationAction
 							key={link.title}
-							component={Link}
+							component={RouterLink}
 							to={link.url}
 							label={link.title}
 							icon={link.icon}
 						/>
 					))}
 				</BottomNavigation>
-			</Box>
+			</Paper>
 			<Toolbar />
 		</>
 	);
